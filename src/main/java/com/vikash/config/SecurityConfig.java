29dc002a9fba312ea3ed9509464f354cda216ce2
Vikash <svikash120@gmail.com>
2020-06-20 17:29:09 +0530

    Implemented authentication from database

--- src/main/java/com/vikash/config/SecurityConfig.java
+++ src/main/java/com/vikash/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.vikash.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,11 +11,21 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	DataSource dataSource;
 	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("Vikash").password("{noop}Vik@123").roles("USER").and().withUser("San")
-		.password("{noop}San@123").roles("Admin");
+		/*
+		 * auth.inMemoryAuthentication().withUser("Vikash").password("{noop}Vik@123").
+		 * roles("USER").and().withUser("San")
+		 * .password("{noop}San@123").roles("Admin");
+		 */
+		  auth.jdbcAuthentication().dataSource(dataSource).
+		  usersByUsernameQuery("SELECT username,password,enabled FROM user WHERE username=?"
+		  ).
+		  authoritiesByUsernameQuery("SELECT username,role FROM user_roles WHERE username=?"
+		  );
 	}
 	
 	@Override
